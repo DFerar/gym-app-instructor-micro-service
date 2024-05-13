edeecdff0e5c811e561e7ea1afdbf2ef78207288
DenisFerar <mr.ferar@gmail.com>
2024-05-13 15:29:49 +0400

    first commit

---
+++ src/test/java/com/gym/gymmicroservice/GymMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.gymmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
