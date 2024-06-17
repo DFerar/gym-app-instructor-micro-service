29f9dac5b57f1d7c25ac86647ed8b1a1ea906c26
DFerar <mr.ferar@gmail.com>
2024-06-17 12:54:02 +0400

    Done kafka communication with core service via application.yml

--- src/main/java/com/gym/gymmicroservice/service/KafkaConsumerService.java
+++ src/main/java/com/gym/gymmicroservice/service/KafkaConsumerService.java
@@ -1,9 +1,13 @@
 package com.gym.gymmicroservice.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gym.gymmicroservice.dto.request.AcceptWorkloadRequestDto;
 import com.gym.gymmicroservice.mapper.AcceptWorkloadMapper;
+
 import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -14,12 +18,19 @@ import org.springframework.stereotype.Service;
 public class KafkaConsumerService {
     private final AcceptWorkloadMapper acceptWorkloadMapper;
     private final WorkloadCalculationService workloadCalculationService;
+    private final ObjectMapper objectMapper;
 
     @KafkaListener(topics = "gym-topic", groupId = "gym-group")
-    public void consume(Map<Object, Object> map) {
-//        var entity = acceptWorkloadMapper.dtoToEntity(dto);
-//        workloadCalculationService.addWorkload(entity, dto.getTrainingDate(), dto.getTrainingDuration(),
-//            dto.getActionType());
-        log.info("Consumed message: {}", map);
+    public void consume(String request) {
+        var dto = convertStringToObject(request);
+        var entity = acceptWorkloadMapper.dtoToEntity(dto);
+        workloadCalculationService.addWorkload(entity, dto.getTrainingDate(), dto.getTrainingDuration(),
+                dto.getActionType());
+        log.info("Consumed message: {}", request);
+    }
+
+    @SneakyThrows
+    private AcceptWorkloadRequestDto convertStringToObject(String request) {
+        return objectMapper.readValue(request, AcceptWorkloadRequestDto.class);
     }
 }
--- src/test/java/com/gym/gymmicroservice/service/WorkloadCalculationServiceTest.java
+++ src/test/java/com/gym/gymmicroservice/service/WorkloadCalculationServiceTest.java
@@ -14,7 +14,8 @@ import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import org.apache.commons.lang.RandomStringUtils;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
